11771edc73000aad60124c1b2da7c767fa2693d3
Anshuman Tripathy <78646817+Anshuman115@users.noreply.github.com>
2023-01-28 18:32:29 +0530

    project initialized & model, basic controller made

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    DB_LOCAL_URI: "mongodb://0.0.0.0:27017/stayin",
+  },
+};
