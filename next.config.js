75596b15971aae745ab845ce681756c56e1ed20d
anshuman115 <anshumantripathy123@gmail.com>
2023-06-05 08:47:25 +0530

    fix next.config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
